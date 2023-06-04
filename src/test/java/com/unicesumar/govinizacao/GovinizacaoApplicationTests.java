e17d448213a40ffdd0a9112605d774034e730220
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2023-06-04 12:18:15 -0300

    Inicio do novo projeto com mongoDB e API

---
+++ src/test/java/com/unicesumar/govinizacao/GovinizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicesumar.govinizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovinizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
